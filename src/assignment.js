7dcf1a1586dd1c137f407d8bad18e7718d8365d6
Atinuke Wale-Awe <108989795+altinuke@users.noreply.github.com>
2022-07-09 18:43:19 +0000

    attempt-2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0
+  for (let currentNumber of arrayOfNumbers) {
+    sum = sum + currentNumber
+  }
+  return sum;
+   
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,17 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let count = 0 
+  for (currentNumber of arrayOfNumbers){
+    let isEven = currentNumber % 2 == 0
+    if (isEven){
+    count= count + 1
+    }
+    
+  }
+ return count  
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -46,10 +59,19 @@ function countEvenNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
  */
-function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+function celsiusToFahrenheit(arrayOfNumbers){
+    let convertedNumbers = []
+  for(let number of arrayOfNumbers){
+    let formula= (number * 9/5) + 32
+   formula= Math.trunc(formula)
+    convertedNumbers.push(formula)
+    
+    }
+  return convertedNumbers
+   
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
